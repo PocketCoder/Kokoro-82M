fe87c9e17dcf199d287f97f152deb3acf5cc1028
PocketCoder <jake.williams77@gmail.com>
2025-01-25 09:01:49 +0000

    Prettier

--- kokoro.py
+++ kokoro.py
@@ -5,88 +5,110 @@ import sys
 import numpy as np
 from voices import VoiceName, LanguageCode
 
-if sys.platform.startswith('darwin'):
-    print('MacOS detected')
+if sys.platform.startswith("darwin"):
+    print("MacOS detected")
     try:
         from phonemizer.backend.espeak.wrapper import EspeakWrapper
-        EspeakWrapper.set_library('/opt/homebrew/Cellar/espeak-ng/1.52.0/lib/libespeak-ng.1.dylib')
+
+        EspeakWrapper.set_library(
+            "/opt/homebrew/Cellar/espeak-ng/1.52.0/lib/libespeak-ng.1.dylib"
+        )
     except Exception as e:
         print(e)
-        print('Failed to set espeak-ng library path. Try running "brew install espeak-ng"')
+        print(
+            'Failed to set espeak-ng library path. Try running "brew install espeak-ng"'
+        )
+
 
 def split_num(num):
     num = num.group()
-    if '.' in num:
+    if "." in num:
         return num
-    elif ':' in num:
-        h, m = [int(n) for n in num.split(':')]
+    elif ":" in num:
+        h, m = [int(n) for n in num.split(":")]
         if m == 0:
             return f"{h} o'clock"
         elif m < 10:
-            return f'{h} oh {m}'
-        return f'{h} {m}'
+            return f"{h} oh {m}"
+        return f"{h} {m}"
     year = int(num[:4])
     if year < 1100 or year % 1000 < 10:
         return num
     left, right = num[:2], int(num[2:4])
-    s = 's' if num.endswith('s') else ''
+    s = "s" if num.endswith("s") else ""
     if 100 <= year % 1000 <= 999:
         if right == 0:
-            return f'{left} hundred{s}'
+            return f"{left} hundred{s}"
         elif right < 10:
-            return f'{left} oh {right}{s}'
-    return f'{left} {right}{s}'
+            return f"{left} oh {right}{s}"
+    return f"{left} {right}{s}"
+
 
 def flip_money(m):
     m = m.group()
-    bill = 'dollar' if m[0] == '$' else 'pound'
+    bill = "dollar" if m[0] == "$" else "pound"
     if m[-1].isalpha():
-        return f'{m[1:]} {bill}s'
-    elif '.' not in m:
-        s = '' if m[1:] == '1' else 's'
-        return f'{m[1:]} {bill}{s}'
-    b, c = m[1:].split('.')
-    s = '' if b == '1' else 's'
-    c = int(c.ljust(2, '0'))
-    coins = f"cent{'' if c == 1 else 's'}" if m[0] == '$' else ('penny' if c == 1 else 'pence')
-    return f'{b} {bill}{s} and {c} {coins}'
+        return f"{m[1:]} {bill}s"
+    elif "." not in m:
+        s = "" if m[1:] == "1" else "s"
+        return f"{m[1:]} {bill}{s}"
+    b, c = m[1:].split(".")
+    s = "" if b == "1" else "s"
+    c = int(c.ljust(2, "0"))
+    coins = (
+        f"cent{'' if c == 1 else 's'}"
+        if m[0] == "$"
+        else ("penny" if c == 1 else "pence")
+    )
+    return f"{b} {bill}{s} and {c} {coins}"
+
 
 def point_num(num):
-    a, b = num.group().split('.')
-    return ' point '.join([a, ' '.join(b)])
+    a, b = num.group().split(".")
+    return " point ".join([a, " ".join(b)])
+
 
 def normalize_text(text):
     text = text.replace(chr(8216), "'").replace(chr(8217), "'")
-    text = text.replace('«', chr(8220)).replace('»', chr(8221))
+    text = text.replace("«", chr(8220)).replace("»", chr(8221))
     text = text.replace(chr(8220), '"').replace(chr(8221), '"')
-    text = text.replace('(', '«').replace(')', '»')
-    for a, b in zip('、。！，：；？', ',.!,:;?'):
-        text = text.replace(a, b+' ')
-    text = re.sub(r'[^\S \n]', ' ', text)
-    text = re.sub(r'  +', ' ', text)
-    text = re.sub(r'(?<=\n) +(?=\n)', '', text)
-    text = re.sub(r'\bD[Rr]\.(?= [A-Z])', 'Doctor', text)
-    text = re.sub(r'\b(?:Mr\.|MR\.(?= [A-Z]))', 'Mister', text)
-    text = re.sub(r'\b(?:Ms\.|MS\.(?= [A-Z]))', 'Miss', text)
-    text = re.sub(r'\b(?:Mrs\.|MRS\.(?= [A-Z]))', 'Mrs', text)
-    text = re.sub(r'\betc\.(?! [A-Z])', 'etc', text)
-    text = re.sub(r'(?i)\b(y)eah?\b', r"\1e'a", text)
-    text = re.sub(r'\d*\.\d+|\b\d{4}s?\b|(?<!:)\b(?:[1-9]|1[0-2]):[0-5]\d\b(?!:)', split_num, text)
-    text = re.sub(r'(?<=\d),(?=\d)', '', text)
-    text = re.sub(r'(?i)[$£]\d+(?:\.\d+)?(?: hundred| thousand| (?:[bm]|tr)illion)*\b|[$£]\d+\.\d\d?\b', flip_money, text)
-    text = re.sub(r'\d*\.\d+', point_num, text)
-    text = re.sub(r'(?<=\d)-(?=\d)', ' to ', text)
-    text = re.sub(r'(?<=\d)S', ' S', text)
+    text = text.replace("(", "«").replace(")", "»")
+    for a, b in zip("、。！，：；？", ",.!,:;?"):
+        text = text.replace(a, b + " ")
+    text = re.sub(r"[^\S \n]", " ", text)
+    text = re.sub(r"  +", " ", text)
+    text = re.sub(r"(?<=\n) +(?=\n)", "", text)
+    text = re.sub(r"\bD[Rr]\.(?= [A-Z])", "Doctor", text)
+    text = re.sub(r"\b(?:Mr\.|MR\.(?= [A-Z]))", "Mister", text)
+    text = re.sub(r"\b(?:Ms\.|MS\.(?= [A-Z]))", "Miss", text)
+    text = re.sub(r"\b(?:Mrs\.|MRS\.(?= [A-Z]))", "Mrs", text)
+    text = re.sub(r"\betc\.(?! [A-Z])", "etc", text)
+    text = re.sub(r"(?i)\b(y)eah?\b", r"\1e'a", text)
+    text = re.sub(
+        r"\d*\.\d+|\b\d{4}s?\b|(?<!:)\b(?:[1-9]|1[0-2]):[0-5]\d\b(?!:)", split_num, text
+    )
+    text = re.sub(r"(?<=\d),(?=\d)", "", text)
+    text = re.sub(
+        r"(?i)[$£]\d+(?:\.\d+)?(?: hundred| thousand| (?:[bm]|tr)illion)*\b|[$£]\d+\.\d\d?\b",
+        flip_money,
+        text,
+    )
+    text = re.sub(r"\d*\.\d+", point_num, text)
+    text = re.sub(r"(?<=\d)-(?=\d)", " to ", text)
+    text = re.sub(r"(?<=\d)S", " S", text)
     text = re.sub(r"(?<=[BCDFGHJ-NP-TV-Z])'?s\b", "'S", text)
-    text = re.sub(r"(?<=X')S\b", 's', text)
-    text = re.sub(r'(?:[A-Za-z]\.){2,} [a-z]', lambda m: m.group().replace('.', '-'), text)
-    text = re.sub(r'(?i)(?<=[A-Z])\.(?=[A-Z])', '-', text)
+    text = re.sub(r"(?<=X')S\b", "s", text)
+    text = re.sub(
+        r"(?:[A-Za-z]\.){2,} [a-z]", lambda m: m.group().replace(".", "-"), text
+    )
+    text = re.sub(r"(?i)(?<=[A-Z])\.(?=[A-Z])", "-", text)
     return text.strip()
+
 
 def get_vocab():
     _pad = "$"
     _punctuation = ';:,.!?¡¿—…"«»“” '
-    _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
     symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
     dicts = {}
@@ -94,39 +116,58 @@ def get_vocab():
         dicts[symbols[i]] = i
     return dicts
 
+
 VOCAB = get_vocab()
+
+
 def tokenize(ps):
     return [i for i in map(VOCAB.get, ps) if i is not None]
 
+
 phonemizers = dict(
-    a=phonemizer.backend.EspeakBackend(language='en-us', preserve_punctuation=True, with_stress=True),
-    b=phonemizer.backend.EspeakBackend(language='en-gb', preserve_punctuation=True, with_stress=True),
+    a=phonemizer.backend.EspeakBackend(
+        language="en-us", preserve_punctuation=True, with_stress=True
+    ),
+    b=phonemizer.backend.EspeakBackend(
+        language="en-gb", preserve_punctuation=True, with_stress=True
+    ),
 )
+
+
 def phonemize(text: str, lang: LanguageCode, norm: bool | None = True) -> str:
     if norm:
         text = normalize_text(text)
     ps = phonemizers[lang].phonemize([text])
-    ps = ps[0] if ps else ''
+    ps = ps[0] if ps else ""
     # https://en.wiktionary.org/wiki/kokoro#English
-    ps = ps.replace('kəkˈoːɹoʊ', 'kˈoʊkəɹoʊ').replace('kəkˈɔːɹəʊ', 'kˈəʊkəɹəʊ')
-    ps = ps.replace('ʲ', 'j').replace('r', 'ɹ').replace('x', 'k').replace('ɬ', 'l')
-    ps = re.sub(r'(?<=[a-zɹː])(?=hˈʌndɹɪd)', ' ', ps)
-    ps = re.sub(r' z(?=[;:,.!?¡¿—…"«»“” ]|$)', 'z', ps)
-    if lang == 'a':
-        ps = re.sub(r'(?<=nˈaɪn)ti(?!ː)', 'di', ps)
-    ps = ''.join(filter(lambda p: p in VOCAB, ps))
+    ps = ps.replace("kəkˈoːɹoʊ", "kˈoʊkəɹoʊ").replace("kəkˈɔːɹəʊ", "kˈəʊkəɹəʊ")
+    ps = ps.replace("ʲ", "j").replace("r", "ɹ").replace("x", "k").replace("ɬ", "l")
+    ps = re.sub(r"(?<=[a-zɹː])(?=hˈʌndɹɪd)", " ", ps)
+    ps = re.sub(r' z(?=[;:,.!?¡¿—…"«»“” ]|$)', "z", ps)
+    if lang == "a":
+        ps = re.sub(r"(?<=nˈaɪn)ti(?!ː)", "di", ps)
+    ps = "".join(filter(lambda p: p in VOCAB, ps))
     return ps.strip()
 
+
 def length_to_mask(lengths: torch.Tensor) -> torch.Tensor:
-    mask = torch.arange(lengths.max()).unsqueeze(0).expand(lengths.shape[0], -1).type_as(lengths)
-    mask = torch.gt(mask+1, lengths.unsqueeze(1))
+    mask = (
+        torch.arange(lengths.max())
+        .unsqueeze(0)
+        .expand(lengths.shape[0], -1)
+        .type_as(lengths)
+    )
+    mask = torch.gt(mask + 1, lengths.unsqueeze(1))
     return mask
 
+
 @torch.no_grad()
-def forward(model: torch.nn.Module, 
-           tokens: torch.Tensor, 
-           ref_s: torch.Tensor | None = None, 
-           speed: float = 1.0) -> np.ndarray:
+def forward(
+    model: torch.nn.Module,
+    tokens: torch.Tensor,
+    ref_s: torch.Tensor | None = None,
+    speed: float = 1.0,
+) -> np.ndarray:
     device = ref_s.device
     tokens = torch.LongTensor([[0, *tokens, 0]]).to(device)
     input_lengths = torch.LongTensor([tokens.shape[-1]]).to(device)
@@ -142,31 +183,36 @@ def forward(model: torch.nn.Module,
     pred_aln_trg = torch.zeros(input_lengths, pred_dur.sum().item())
     c_frame = 0
     for i in range(pred_aln_trg.size(0)):
-        pred_aln_trg[i, c_frame:c_frame + pred_dur[0,i].item()] = 1
-        c_frame += pred_dur[0,i].item()
+        pred_aln_trg[i, c_frame : c_frame + pred_dur[0, i].item()] = 1
+        c_frame += pred_dur[0, i].item()
     en = d.transpose(-1, -2) @ pred_aln_trg.unsqueeze(0).to(device)
     F0_pred, N_pred = model.predictor.F0Ntrain(en, s)
     t_en = model.text_encoder(tokens, input_lengths, text_mask)
     asr = t_en @ pred_aln_trg.unsqueeze(0).to(device)
     return model.decoder(asr, F0_pred, N_pred, ref_s[:, :128]).squeeze().cpu().numpy()
 
-def generate(model: torch.nn.Module,
-            text: str,
-            voicepack: VoiceName,
-            lang: LanguageCode = 'a',
-            speed: float = 1,
-            ps: str | None = None) -> tuple[np.ndarray, str] | None:
+
+def generate(
+    model: torch.nn.Module,
+    text: str,
+    voicepack: VoiceName,
+    lang: LanguageCode = "a",
+    speed: float = 1,
+    ps: str | None = None,
+) -> tuple[np.ndarray, str] | None:
     ps = ps or phonemize(text, lang)
     tokens = tokenize(ps)
     if not tokens:
         return None
     elif len(tokens) > 510:
         tokens = tokens[:510]
-        print('Truncated to 510 tokens')
+        print("Truncated to 510 tokens")
     ref_s = voicepack[len(tokens)]
     out = forward(model, tokens, ref_s, speed)
-    ps = ''.join(next(k for k, v in VOCAB.items() if i == v) for i in tokens)
+    ps = "".join(next(k for k, v in VOCAB.items() if i == v) for i in tokens)
     return out, ps
+
+
 def generate_full(model, text, voicepack, lang="a", speed=1, ps=None):
     ps = ps or phonemize(text, lang)
     tokens = tokenize(ps)
